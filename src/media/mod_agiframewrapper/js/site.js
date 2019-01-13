c9d1dd04d9ca668e99bcb5ef481f68a6ab6e13cd
astridx <info@astrid-guenther.de>
2019-01-13 20:54:07 +0100

    wip

--- src/media/mod_agiframewrapper/js/site.js
+++ src/media/mod_agiframewrapper/js/site.js
@@ -1,14 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
 	var agiframewrappers = document.querySelectorAll('.agiframewrapper');
 
-	// For all agiframewrappers [start]
 	[].forEach.call(agiframewrappers, function (element) {
 
             var modulid = element.getAttribute('data-modul_id');
             var url_active = element.getAttribute('data-url_active');
             var url_deactive = element.getAttribute('data-url_deactive');
             
-            //alert(modulid);
             var activate = document.getElementById('agenable-' + modulid);
             var deactivate = document.getElementById('agdisable-' + modulid);
             
